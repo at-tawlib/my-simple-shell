66cc02918246ecc052b90a60c518244ca84b45ff
at-tawlib <a.fatahu95@gmail.com>
2022-11-13 18:45:44 +0000

    Initialize our symbol table stack

---
+++ initsh.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+#include "symtab/symtab.h"
+
+extern char **environ;
+
+/**
+ * initsh - initializes the symbol table stack (including the global
+ * symbol table) and scans the environment list, adding each environment
+ * variable to the table.
+ */
+void initsh(void)
+{
+	init_symtab();
+
+	struct symtab_entry_s *entry;
+	char **p2 = environ;
+
+	while (*p2)
+	{
+		char *eq = strchr(*p2, '=');
+
+		if (eq)
+		{
+			int len = eq - (*p2);
+			char name[len + 1];
+
+			strncpy(name, *p2, len);
+			name[len] = '\0';
+			entry = add_to_symtab(name);
+
+			if (entry)
+			{
+				symtab_entry_setval(entry, eq + 1);
+				entry->flags |= FLAG_EXPORT;
+			}
+		}
+		else
+		{
+			entry = add_to_symtab(*p2);
+		}
+		p2++;
+	}
+
+	entry = add_to_symtab("PS1");
+	symtab_entry_setval(entry, "$ ");
+
+	entry = add_to_symtab("PS2");
+	symtab_entry_setval(entry, "> ");
+}
