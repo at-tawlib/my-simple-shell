b31e1811c74abfb9e2359cecf89a98a95bf26366
at-tawlib <a.fatahu95@gmail.com>
2022-11-13 19:17:33 +0000

    Update the prompt printing functions

--- prompt.c
+++ prompt.c
@@ -1,12 +1,27 @@
 #include "shell.h"
+#include "symtab/symtab.h"
 
 /**
  * print_prompt1 - prints normal prompt
  * Return: void
+ *
+ * Update -  checks if there is a symbol table entry with the name PS!,
+ * if there is we use that entry's value to print the first prompt string.
+ * Otherwise, we use our default builtin value, which is $
  */
 void print_prompt1(void)
 {
-	fprintf(stderr, "$ ");
+	/* fprintf(stderr, "$ "); */
+	struct symtab_entry_s *entry = get_symtab_entry("PS1");
+
+	if (entry && entry->val)
+	{
+		fprintf(stderr, "%s", entry->val);
+	}
+	else
+	{
+		fprintf(stderr, "$ ");
+	}
 }
 
 /**
@@ -15,5 +30,15 @@ void print_prompt1(void)
  */
 void print_prompt2(void)
 {
-	fprintf(stderr, "> ");
+	/* fprintf(stderr, "> "); */
+	struct symtab_entry_s *entry = get_symtab_entry("PS2");
+
+	if (entry && entry->val)
+	{
+		fprintf(stderr, "%s", entry->val);
+	}
+	else
+	{
+		fprintf(stderr, "> ");
+	}
 }
